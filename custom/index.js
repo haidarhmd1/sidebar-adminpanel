6f0ea8a56a9d14c390d070d08688ac9da74dea93
Haidar Hammoud <haidar.hmd1@gmail.com>
2018-02-13 23:36:01 +0200

    added topnav functionality with msg and profile, fixes and jscript enhancement

--- custom/index.js
+++ custom/index.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-
-    $("#side_bar_toggle").click(function(e){
-        e.preventDefault();
-        $(".sidebar_menu").toggleClass("main-screen-full");
-        $(".main_content , #notification-content").toggleClass("content_side_padding");
-    });
-
+        toggleItems("#side_bar_toggle",".sidebar_menu","main-screen-full"); //SideBar Menu
+        toggleItems("#side_bar_toggle",".main_content , #notification-content","content_side_padding"); //SideBar Menu
+        toggleItems("#show-content",".dropdrown-nav","show-hidden-menu"); //DropDown Menu
+        toggleItems("#account-content",".dropdrown-nav-account","show-hidden-menu"); //DropDown Menu
+        toggleItems("#user-settings",".show_container_settings","settings-container"); //Settings Container topNav
+        toggleItems("#msg-show",".show_container_msg","msg-container"); //Msg Container TopNav
 });
+
+function toggleItems(clickItem,firstObject, secondObject){
+    $(clickItem).click(function(e){
+        $(firstObject).toggleClass(secondObject);
+    });
+}
